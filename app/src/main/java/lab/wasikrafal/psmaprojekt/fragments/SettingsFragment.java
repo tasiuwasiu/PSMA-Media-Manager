ba983090f54c3d31c1625900518aaf572a3673c5
tasiuwasiu <tasiu24@gmail.com>
2018-06-15 13:11:31 +0200

    WIP

--- app/src/main/java/lab/wasikrafal/psmaprojekt/fragments/SettingsFragment.java
+++ app/src/main/java/lab/wasikrafal/psmaprojekt/fragments/SettingsFragment.java
@@ -1,5 +1,6 @@
 package lab.wasikrafal.psmaprojekt.fragments;
 
+import android.app.Dialog;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -7,7 +8,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import java.util.Arrays;
 import java.util.List;
@@ -23,6 +27,9 @@ public class SettingsFragment extends Fragment
 
     int videoSort;
     int audioSort;
+
+    String[] sortTypes = {"Tytuł", "Data wykonania", "Długość"};
+    int[] sortCodes = {MainActivity.SORT_NAME, MainActivity.SORT_DATE, MainActivity.SORT_LENGTH};
 
     @Override
     public void onCreate(Bundle bundle)
@@ -83,9 +90,6 @@ public class SettingsFragment extends Fragment
         return view;
     }
 
-
-
-
     private void loadData()
     {
         SharedPreferences sharedPreferences = getActivity().getSharedPreferences("preferences", Context.MODE_PRIVATE);
@@ -106,33 +110,100 @@ public class SettingsFragment extends Fragment
     private void setVideoSorting()
     {
         //dialog
+        final Dialog dialog = new Dialog(getActivity());
+        dialog.setContentView(R.layout.dialog_change_sort);
 
+        final Spinner spinner = (Spinner) dialog.findViewById(R.id.sp_sort_types);
+        ArrayAdapter spinnerArrayAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_spinner_dropdown_item, sortTypes);
+        spinner.setAdapter(spinnerArrayAdapter);
 
+        Button okButton = (Button) dialog.findViewById(R.id.but_set_sort_ok);
+        okButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                videoSort = sortCodes[spinner.getSelectedItemPosition()];
+                main.setVideoSort(videoSort);
+                saveData();
+                dialog.dismiss();
+            }
+        });
 
-        main.setVideoSort(videoSort);
-        saveData();
+        dialog.show();
+        Window window = dialog.getWindow();
+        window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
     }
 
     private void setAudioSorting()
     {
         //dialog
+        final Dialog dialog = new Dialog(getActivity());
+        dialog.setContentView(R.layout.dialog_change_sort);
 
+        final Spinner spinner = (Spinner) dialog.findViewById(R.id.sp_sort_types);
+        ArrayAdapter spinnerArrayAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_spinner_dropdown_item, sortTypes);
+        spinner.setAdapter(spinnerArrayAdapter);
 
-        main.setAudioSort(audioSort);
-        saveData();
+        Button okButton = (Button) dialog.findViewById(R.id.but_set_sort_ok);
+        okButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                audioSort = sortCodes[spinner.getSelectedItemPosition()];
+                main.setAudioSort(audioSort);
+                saveData();
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+        Window window = dialog.getWindow();
+        window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
     }
 
     private void resetCategories()
     {
         ///dialog
+        final Dialog dialog = new Dialog(getActivity());
+        dialog.setContentView(R.layout.dialog_set_reset_confirm);
 
-        MediaDatabase database = MediaDatabase.getInstance(getActivity());
-        List<AudioCategory> list = Arrays.asList(database.audioCategoryDAO().loadAllCategories());
-        for (AudioCategory ac: list)
+        Button okButton = (Button) dialog.findViewById(R.id.but_set_conf_ok);
+        okButton.setOnClickListener(new View.OnClickListener()
         {
-            database.audioCategoryDAO().deleteCategory(ac);
-        }
-        database.audioCategoryDAO().insertAll(AudioCategory.populateData());
+            @Override
+            public void onClick(View view)
+            {
+
+                MediaDatabase database = MediaDatabase.getInstance(getActivity());
+                List<AudioCategory> list = Arrays.asList(database.audioCategoryDAO().loadAllCategories());
+                for (AudioCategory ac: list)
+                {
+                    database.audioCategoryDAO().deleteCategory(ac);
+                }
+                database.audioCategoryDAO().insertAll(AudioCategory.populateData());
+                dialog.dismiss();
+            }
+        });
+
+        Button noButton = (Button) dialog.findViewById(R.id.but_set_conf_no);
+        noButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+        Window window = dialog.getWindow();
+        window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+
+
 
     }
 
